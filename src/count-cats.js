8abd1a3138e69f5e7e339040a58420ec780b4a6a
Malvina Kochkina <malvina.kochkina@instech.no>
2020-09-30 15:13:33 +0200

    feat: solve carbon-dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 const CustomError = require('../extensions/custom-error');
 
 module.exports = function countCats(array) {
-  throw new CustomError('Not implemented');
+  //throw new CustomError('Not implemented');
   let count = 0;
   for (let i = 0; i < array.length; i++) {
     if (array[i].includes('^^')) {
