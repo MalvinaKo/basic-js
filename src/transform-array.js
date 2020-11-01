4541ab2414341ef5121d99b7c3bffcdae7d47716
Malvina Kochkina <malvina.kochkina@instech.no>
2020-11-01 16:10:28 +0100

    fix code

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,23 +2,24 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   //throw new CustomError('Not implemented');
-  for(let i = 0; i < arr.length; i++){ 
-    if (arr[arr.length-1]==='--discard-next' || arr[arr.length-1]==='--double-next') arr.splice(arr.length-1,1);
-    else if (arr[0]==='--discard-prev' || arr[0]==='--double-prev') arr.splice(0,1);
+  const newArr=arr;
+  for(let i = 0; i < newArr.length; i++){ 
+    if (newArr[newArr.length-1]==='--discard-next' || newArr[newArr.length-1]==='--double-next') newArr.splice(newArr.length-1,1);
+    else if (newArr[0]==='--discard-prev' || newArr[0]==='--double-prev') newArr.splice(0,1);
     else 
-      if ( arr[i] === '--discard-next' ) { 
-        arr.splice(i, 2); 
+      if ( newArr[i] === '--discard-next' ) { 
+        newArr.splice(i, 2); 
         }
-      else if ( arr[i] === '--discard-prev' ) { 
-        arr.splice(i-1, 2);
+      else if ( newArr[i] === '--discard-prev' ) { 
+        newArr.splice(i-1, 2);
         }
-      else if ( arr[i] === '--double-next' ) {
-        arr.splice(i, 1, arr[i+1]); 
+      else if ( newArr[i] === '--double-next' ) {
+        newArr.splice(i, 1, newArr[i+1]); 
         }
-      else if ( arr[i] === '--double-prev' ) { 
-        arr.splice(i, 1, arr[i-1]); 
+      else if ( newArr[i] === '--double-prev' ) { 
+        newArr.splice(i, 1, newArr[i-1]); 
         }
     };
       
-    return arr
+    return newArr
     };
