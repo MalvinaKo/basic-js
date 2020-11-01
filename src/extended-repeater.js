200b6f8ead38a22e1d37ffb3f651a53b5ae63f42
Malvina Kochkina <malvina.kochkina@instech.no>
2020-11-01 17:15:57 +0100

    solve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,15 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  const string = str;
+  const string = str ? str.toString() : 
+  str===null ? 'null' : '';
   const repeatTimes = options.repeatTimes ? parseInt(options.repeatTimes) : 1;
   const separator = options.separator ? options.separator.toString() : '+';
+
+  const addition = options.addition ? options.addition.toString() 
+  : (typeof options.addition === 'boolean' && !!options.addition===false  && options.addition!==null) ? 'false' 
+  : options.addition !== null ? '' : 'null';  
+  
   const additionRepeatTimes = options.additionRepeatTimes ? parseInt(options.additionRepeatTimes) : 1;
   const additionSeparator = options.additionSeparator ? options.additionSeparator.toString() : '|';
-  
-  const addition = options.addition ? options.addition.toString() : '';
-  
 
 
 return (string + ((addition + additionSeparator).repeat(additionRepeatTimes).slice(0,-(additionSeparator.length))  + separator)).repeat(repeatTimes).slice(0, -(separator.length))
+
 };
